a1b1b5f345cf17228ecc3bc13d8dd90ef7304702
Joshua Warren <josh@creatuity.com>
2015-04-18 19:47:08 -0700

    Initial working version

---
+++ Vagrantfile
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# To install store sample data
+sample_data = "true"
+
+Vagrant.configure("2") do |config|
+  # All Vagrant configuration is done here. The most common configuration
+  # options are documented and commented below. For a complete reference,
+  # please see the online documentation at vagrantup.com.
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "ubuntu/trusty64"
+  
+  config.vm.provision :shell, :path => "bootstrap.sh", :args => [sample_data]
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=777","fmode=666"]
+
+  config.ssh.forward_agent = true
+
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+    vb.name = "blackfire-magento"
+  end
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+
+SAMPLE_DATA=$1
+MAGE_VERSION="1.9.1.0"
+DATA_VERSION="1.9.0.0"
+
+# Update Apt
+# --------------------
+apt-get update
+
+# Install Apache & PHP
+# --------------------
+apt-get install -y apache2
+apt-get install -y php5
+apt-get install -y libapache2-mod-php5
+apt-get install -y php5-mysqlnd php5-curl php5-xdebug php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-soap
+
+php5enmod mcrypt
+
+# Delete default apache web dir and symlink mounted vagrant dir from host machine
+# --------------------
+rm -rf /var/www/html
+mkdir /vagrant/httpdocs
+ln -fs /vagrant/httpdocs /var/www/html
+
+# Replace contents of default Apache vhost
+# --------------------
+VHOST=$(cat <<EOF
+NameVirtualHost *:8080
+Listen 8080
+<VirtualHost *:80>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+<VirtualHost *:8080>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+)
+
+echo "$VHOST" > /etc/apache2/sites-enabled/000-default.conf
+
+a2enmod rewrite
+service apache2 restart
+
+# Mysql
+# --------------------
+# Ignore the post install questions
+export DEBIAN_FRONTEND=noninteractive
+# Install MySQL quietly
+apt-get -q -y install mysql-server-5.5
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS magentodb"
+mysql -u root -e "GRANT ALL PRIVILEGES ON magentodb.* TO 'magentouser'@'localhost' IDENTIFIED BY 'password'"
+mysql -u root -e "FLUSH PRIVILEGES"
+
+
+# Magento
+# --------------------
+# http://www.magentocommerce.com/wiki/1_-_installation_and_configuration/installing_magento_via_shell_ssh
+
+# Download and extract
+if [[ ! -f "/vagrant/httpdocs/index.php" ]]; then
+  cd /vagrant/httpdocs
+  wget http://www.magentocommerce.com/downloads/assets/${MAGE_VERSION}/magento-${MAGE_VERSION}.tar.gz
+  tar -zxvf magento-${MAGE_VERSION}.tar.gz
+  mv magento/* magento/.htaccess .
+  chmod -R o+w media var
+  chmod o+w app/etc
+  # Clean up downloaded file and extracted dir
+  rm -rf magento*
+fi
+
+
+# Sample Data
+if [[ $SAMPLE_DATA == "true" ]]; then
+  cd /vagrant
+
+  if [[ ! -f "/vagrant/magento-sample-data-${DATA_VERSION}.tar.gz" ]]; then
+    # Only download sample data if we need to
+    wget http://www.magentocommerce.com/downloads/assets/${DATA_VERSION}/magento-sample-data-${DATA_VERSION}.tar.gz
+  fi
+
+  tar -zxvf magento-sample-data-${DATA_VERSION}.tar.gz
+  cp -R magento-sample-data-${DATA_VERSION}/media/* httpdocs/media/
+  cp -R magento-sample-data-${DATA_VERSION}/skin/*  httpdocs/skin/
+  mysql -u root magentodb < magento-sample-data-${DATA_VERSION}/magento_sample_data_for_${DATA_VERSION}.sql
+  rm -rf magento-sample-data-${DATA_VERSION}
+fi
+
+
+# Run installer
+if [ ! -f "/vagrant/httpdocs/app/etc/local.xml" ]; then
+  cd /vagrant/httpdocs
+  sudo /usr/bin/php -f install.php -- --license_agreement_accepted yes \
+  --locale en_US --timezone "America/Los_Angeles" --default_currency USD \
+  --db_host localhost --db_name magentodb --db_user magentouser --db_pass password \
+  --url "http://127.0.0.1:8080/" --use_rewrites yes \
+  --use_secure no --secure_base_url "http://127.0.0.1:8080/" --use_secure_admin no \
+  --skip_url_validation yes \
+  --admin_lastname Owner --admin_firstname Store --admin_email "admin@example.com" \
+  --admin_username admin --admin_password password123123
+  /usr/bin/php -f shell/indexer.php reindexall
+fi
+
+# Install n98-magerun
+# --------------------
+cd /vagrant/httpdocs
+wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
+chmod +x ./n98-magerun.phar
+sudo mv ./n98-magerun.phar /usr/local/bin/
+
+wget -O - https://packagecloud.io/gpg.key | apt-key add -
+echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
+apt-get update -y
+apt-get install -y blackfire-agent
+apt-get install -y blackfire-php
+
+echo 'server-id=INSERT-SERVER-ID-HERE' >> /etc/blackfire/agent
+echo 'server-token=INSERT-SERVER-TOKEN-HERE' >> /etc/blackfire/agent
+
+echo 'client-id=INSERT-CLIENT-ID-HERE' >> /root/.blackfire.ini
+echo 'client-token=INSERT-CLIENT-TOKEN-HERE' >> /root/.blackfire.ini
+
+/etc/init.d/blackfire-agent restart
+/etc/init.d/apache2 restart
